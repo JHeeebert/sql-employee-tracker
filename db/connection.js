10e68ef1d8172b9f888ca5b42e2411a6dbf320c6
Joshua Hebert <jhebertcocktails@gmail.com>
2023-07-06 21:04:19 -0400

    added .env

--- db/connection.js
+++ db/connection.js
@@ -6,8 +6,12 @@ const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: process.env.MYSQL_PASSWORD,
+    password: process.env.DB_PASSWORD,
     database: 'employee_tracker_db'
+});
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
 });
 
 module.exports = connection;
